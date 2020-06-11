6c6224e3ec63aa3c009a1880288821d0f137de11
Kiran.Shivalingaiah <Kiran.Shivalingaiah@target.com>
2020-06-11 18:55:12 +0530

    Include micronaut elastic search lib support

--- backpack-elasticsearch-app/src/main/kotlin/com/tgt/backpackelasticsearch/api/controller/BackpackElasticsearchController.kt
+++ backpack-elasticsearch-app/src/main/kotlin/com/tgt/backpackelasticsearch/api/controller/BackpackElasticsearchController.kt
@@ -5,22 +5,13 @@ import com.tgt.backpackelasticsearch.util.BackpackElasticsearchConstants.PROFILE
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.*
 import reactor.core.publisher.Mono
-import java.util.*
 
 @Controller(ELASTIC_SEARCH_BASEPATH)
 class BackpackElasticsearchController() {
-    @Get("/{registry_id}")
-    @Status(HttpStatus.OK)
-    fun getRegistry(
-        @Header(PROFILE_ID) guestId: String,
-        @PathVariable("registry_id") registryId: UUID
-    ): Mono<Void> {
-        return Mono.just(true).then()
-    }
 
     @Get("/name/{name}")
     @Status(HttpStatus.OK)
-    fun searchRegistryByName(
+    fun searchRegistryByFirstSecondName(
         @Header(PROFILE_ID) guestId: String,
         @PathVariable("name") name: String
     ): Mono<Void> {
