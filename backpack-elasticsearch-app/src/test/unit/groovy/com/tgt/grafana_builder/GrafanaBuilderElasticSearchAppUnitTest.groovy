34df8aa1f77646bebd8c13c69acabc881a85c649
Abhishek Kotha <Abhishek.Kotha@target.com>
2021-04-16 13:47:26 -0500

    dashboard changes (#49)

---
+++ backpack-elasticsearch-app/src/test/unit/groovy/com/tgt/grafana_builder/GrafanaBuilderElasticSearchAppUnitTest.groovy
@@ -0,0 +1,46 @@
+package com.tgt.grafana_builder
+
+import com.tgt.grafana_builder.GrafanaBuilder
+import com.tgt.grafana_builder.GrafanaBuilderConfig
+import spock.lang.Ignore
+import spock.lang.Specification
+
+@Ignore
+class GrafanaBuilderElasticSearchAppUnitTest extends Specification {
+
+    def "build backpack-elasticsearch-app grafana dashboard"() {
+        given:
+        def moduleDir = System.getProperty("user.dir")
+
+        def elasticSearchPanels = [
+            new GrafanaBuilderConfig.ElasticSearchPanel(method: "searchRegistry")
+        ]
+
+        def metricsAlert = new GrafanaBuilderConfig.MetricsAlert(
+            prodTapApplication: "backpackelasticsearch",
+            prodTapCluster: "backpackelasticsearch",
+            notificationUids: [ "FQW_lvBZk", "roC6asiMz", "7GGtGwmMz" ],
+            cpuUsageThreshold: "75",
+            memUsageThreshold: "75",
+            server500countThreshold: 25
+        )
+
+        GrafanaBuilderConfig grafanaBuilderConfig = new GrafanaBuilderConfig(
+            tapDashboardJsonFile: "${moduleDir}/src/test/unit/resources/tap-dashboard.json",
+            apiServerSpecBasePath: "/registries_searches/v1",
+            apiServerSpecPath: "${moduleDir}/api-specs/backpack-elasticsearch-v1.yml",
+            httpClientRowTitle: "Outbound Http Clients",
+            needResiliencePanel: true,
+            metricsAlert: metricsAlert,
+            elasticSearchPanels: elasticSearchPanels
+        )
+
+        GrafanaBuilder grafanaBuilder = new GrafanaBuilder(grafanaBuilderConfig)
+
+        when:
+        def success = grafanaBuilder.buildPanels()
+
+        then:
+        success
+    }
+}
---
+++ backpack-elasticsearch-consumer-app/src/test/unit/groovy/com/tgt/grafana_builder/GrafanaBuilderElasticSearchConsumerUnitTest.groovy
@@ -0,0 +1,101 @@
+package com.tgt.grafana_builder
+
+import spock.lang.Ignore
+import spock.lang.Specification
+
+@Ignore
+class GrafanaBuilderElasticSearchConsumerUnitTest extends Specification {
+
+    def "build backpack-elasticsearch-consumer-app grafana dashboard"() {
+        given:
+        def moduleDir = System.getProperty("user.dir")
+
+        def elasticSearchPanels = [
+            new GrafanaBuilderConfig.ElasticSearchPanel(method: "saveRegistry"),
+            new GrafanaBuilderConfig.ElasticSearchPanel(method: "deleteRegistry"),
+            new GrafanaBuilderConfig.ElasticSearchPanel(method: "updateRegistry")
+        ]
+
+        def metricsAlert = new GrafanaBuilderConfig.MetricsAlert(
+            prodTapApplication: "backpackelasticsearchconsumer",
+            prodTapCluster: "backpackelasticsearchconsumer",
+            notificationUids: [ "FQW_lvBZk", "roC6asiMz", "7GGtGwmMz" ],
+            cpuUsageThreshold: "75",
+            memUsageThreshold: "75",
+            server500countThreshold: 25
+        )
+
+        def kafkaConsumers = [
+            new GrafanaBuilderConfig.KafkaConsumer(
+                title: "Elastic Search Consumer",
+                metricsName: "msgbus_consumer_event",
+                isDlqConsumer: false,
+                devEnvironment: new GrafanaBuilderConfig.KafkaConsumerEnvironment(
+                    topic: "registry-internal-data-bus-dev",
+                    consumerGroup: "backpack-elasticsearch-data-bus-dev-consumer",
+                    ttcCluster: "ost-ttc-test-app",
+                    tteCluster: "ost-ttce-test-app"
+                ),
+                stageEnvironment: new GrafanaBuilderConfig.KafkaConsumerEnvironment(
+                    topic: "registry-internal-data-bus-stage",
+                    consumerGroup: "backpack-elasticsearch-data-bus-stage-consumer",
+                    ttcCluster: "ost-ttc-test-app",
+                    tteCluster: "ost-ttce-test-app"
+                ),
+                prodEnvironment: new GrafanaBuilderConfig.KafkaConsumerEnvironment(
+                    topic: "registry-internal-data-bus-prod",
+                    consumerGroup: "backpack-elasticsearch-data-bus-prod-consumer",
+                    ttcCluster: "ost-ttc-prod-app'",
+                    tteCluster: "ost-ttce-prod-app'"
+                )
+            ),
+            new GrafanaBuilderConfig.KafkaConsumer(
+                title: "Elastic Search DLQ Consumer",
+                metricsName: "msgbus_consumer_event",
+                isDlqConsumer: true,
+                devEnvironment: new GrafanaBuilderConfig.KafkaConsumerEnvironment(
+                    topic: "registry-internal-data-bus-dev-dlq",
+                    consumerGroup: "backpack-elasticsearch-data-bus-dev-dlq-consumer",
+                    ttcCluster: "ost-ttc-test-app",
+                    tteCluster: "ost-ttce-test-app"
+                ),
+                stageEnvironment: new GrafanaBuilderConfig.KafkaConsumerEnvironment(
+                    topic: "registry-internal-data-bus-stage-dlq",
+                    consumerGroup: "backpack-elasticsearch-data-bus-stage-dlq-consumer",
+                    ttcCluster: "ost-ttc-test-app",
+                    tteCluster: "ost-ttce-test-app"
+                ),
+                prodEnvironment: new GrafanaBuilderConfig.KafkaConsumerEnvironment(
+                    topic: "registry-internal-data-bus-prod-dlq",
+                    consumerGroup: "backpack-elasticsearch-data-bus-prod-dlq-consumer",
+                    ttcCluster: "ost-ttc-prod-app'",
+                    tteCluster: "ost-ttce-prod-app'"
+                )
+            )
+        ]
+
+        def kafkaProducers = [
+            new GrafanaBuilderConfig.KafkaProducer(
+                title: "Elastic Search msgbus DLQ Producer",
+                metricsName: "msgbus_producer_event",
+                isDlqProducer: true
+            )
+        ]
+
+        GrafanaBuilderConfig grafanaBuilderConfig = new GrafanaBuilderConfig(
+            tapDashboardJsonFile: "${moduleDir}/src/test/unit/resources/tap-dashboard.json",
+            kafkaConsumers: kafkaConsumers,
+            kafkaProducers: kafkaProducers,
+            metricsAlert: metricsAlert,
+            elasticSearchPanels: elasticSearchPanels
+        )
+
+        GrafanaBuilder grafanaBuilder = new GrafanaBuilder(grafanaBuilderConfig)
+
+        when:
+        def success = grafanaBuilder.buildPanels()
+
+        then:
+        success
+    }
+}
